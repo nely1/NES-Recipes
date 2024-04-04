c135b064b6932556de1d3304b79fa6e239ceb739
nely1 <nelsonlynelson@gmail.com>
2024-04-04 12:16:00 +0000

    updated App.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import {useEffect} from "react"
 
 function App() {
 
+  const ROOT = "/"; // the recipes page
+  const INGREDIENTS = "/ingredients";
+  const RECIPE = "/recipe:id"
+
   useEffect(() => {
     fetch("http://localhost:8000/get-all").then(res => res.json()).then(data => {console.log(data)});
     
@@ -11,22 +15,28 @@ function App() {
   ,[])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+    <Routes>
+    <Route path={ROOT} />
+    <Route path={INGREDIENTS}/>  
+    <Route path={RECIPE}/>
+    </Routes></>
   );
 }
 
